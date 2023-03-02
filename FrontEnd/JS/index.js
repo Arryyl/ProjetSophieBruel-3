3342c2ba3dbe5c680866d8439e71f1cba7aa1cf9
Arryyl <ettomaz7@gmail.com>
2023-03-02 12:41:34 +0100

    Changements 1 index.js

--- FrontEnd/JS/index.js
+++ FrontEnd/JS/index.js
@@ -1,10 +1,10 @@
-//declaration
-const categories = "http://localhost:5678/api/categories";
-const works = "http://localhost:5678/api/works";
+const Categories = "http://localhost:5678/api/categories";
+const Works = "http://localhost:5678/api/works";
 
+//declaration categories
 async function getCategories() {
   try {
-    const result = await fetch(categories);
+    const result = await fetch(Categories);
     if (result.ok) {
       const data = await result.json();
       console.log(data);
@@ -15,4 +15,40 @@ async function getCategories() {
     console.log(error);
   }
 }
-getCategories(url);
+getCategories();
+
+//Declaration works
+async function getWorks() {
+  try {
+    const result = await fetch(Works);
+    if (result.ok) {
+      const data = await result.json();
+      console.log(data);
+      return data;
+    } else {
+      console.log("Error !");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//Creation de balises
+async function createGallery() {
+  const works = await getWorks();
+  const figure = works[0];
+
+  const imageElement = document.createElement("img");
+  imageElement.src = figure.image;
+
+  const descriptionElement = document.createElement("figcaption");
+  descriptionElement.innerText = figure.description;
+
+  //rattachement
+  const divGallery = document.querySelector(".gallery");
+
+  divGallery.appendChild(imageElement);
+  divGallery.appendChild(descriptionElement);
+}
+
+createGallery();
